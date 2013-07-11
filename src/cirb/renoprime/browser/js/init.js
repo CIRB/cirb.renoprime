cad294cd342c695a231c7b56dddb3ace4bba0076
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-07-11 12:54:03 +0200

    update gis url, prevent 404 error

--- src/cirb/renoprime/browser/js/init.js
+++ src/cirb/renoprime/browser/js/init.js
@@ -9,13 +9,13 @@ var language=["FR","NL"];
 
 var isoCurrentDate='';
 var isoLowerDate='';
- 
+
 function format_integer(num) {
     var res=''+num;
     if(res.length<2)
         return res='0'+res;
     else
-        return res; 
+        return res;
 }
 
 
@@ -29,10 +29,10 @@ $(document).ready(function() {
     } else {
         LANG = 0;
     }
-    
-    today = $('#map').data('today');                    
+
+    today = $('#map').data('today');
     currentDate=new Date(today);
-    
+
     isoCurrentDate=currentDate.getFullYear()+
                 '-'+format_integer((currentDate.getMonth()+1))+
                 '-'+format_integer(currentDate.getDate());
@@ -43,14 +43,14 @@ $(document).ready(function() {
                 '-'+format_integer((currentDate.getDate()+1));
 
 
-    // prefix_gis_url = $('#map').data('gisurl');                    
+    // prefix_gis_url = $('#map').data('gisurl');
     // address_wb_url = prefix_gis_url+"service/urbis/Rest/Localize/getaddresses";
     address_wb_url = "/gis/service/urbis/Rest/Localize/getaddresses";
     //wms_cirb_url = prefix_gis_url+"geoserver/wms";
     wms_cirb_url = "/gis/geoserver/gwc/service/wms";
 
     prefix_mybrugis_url = $('#map').data('mybrugisurl');
-    urlBrugis = "/proxy/geoserver/ows"; 
+    urlBrugis = "/proxy/geoserver/ows";
 
     init();
 
--- src/cirb/renoprime/browser/js/map.js
+++ src/cirb/renoprime/browser/js/map.js
@@ -4,7 +4,7 @@
  * File: map.js
  *
  * @author fkrzewinski@cirb.irisnet.be inspired by the work of Jean-Paul Dzisiak
- * @version november 2012                                             
+ * @version november 2012
  */
 
 var map, layer, layer_EDRLR, layer_espaces_structurants, layer_Zichee;
@@ -23,7 +23,7 @@ function format_integer(num) {
     if(res.length<2)
         return res='0'+res;
     else
-        return res; 
+        return res;
 }
 
 function init() {
@@ -48,7 +48,7 @@ function init() {
 
     map = new OpenLayers.Map( 'map' ,urbis_options);
 
-    var urbisLayerName=['urbisFR','urbisNL'];               
+    var urbisLayerName=['urbisFR','urbisNL'];
 
     var urbis_layer=new OpenLayers.Layer.WMS(
             urbisLayerName[LANG],
@@ -59,9 +59,9 @@ function init() {
         isBaseLayer: true,
         attribution: "Realized by means of Brussels UrbIS&reg;&copy;"
             }
-            );                              
-    
-    //IE8 fix : 
+            );
+
+    //IE8 fix :
     /*$.get('getToday', function(data,status) {
         console.log('today '+data);
     });*/
@@ -95,7 +95,7 @@ function init() {
     var filterAsString = xml.write(filterAsXml);
 
     layer = new OpenLayers.Layer.WMS( "Contrats de quartier",
-            urlBrugis, 
+            urlBrugis,
             {   layers: 'Contrats_de_quartier',
                 version:'1.3.0',
           transparent: "true",
@@ -109,17 +109,17 @@ function init() {
     layer.params["FILTER"] = filterAsString;
 
     layer_EDRLR = new OpenLayers.Layer.WMS( "EDRLR",
-            urlBrugis, 
+            urlBrugis,
             {   layers: 'EDRLR',
                 version:'1.3.0',
                 transparent: "true",
                 format: 'image/png'
             },
             {isBaseLayer: false}
-            );                      
+            );
 
     layer_batiment=new OpenLayers.Layer.WMS("Batiment Urbis",
-            wms_cirb_url, 
+            "/gis/geoserver/ows",
             {'layers': 'urbis:URB_A_BU', transparent: true, format: 'image/png',visible:false},
             {isBaseLayer: false,visible:false}
             );
@@ -131,35 +131,35 @@ function init() {
     map.addLayer(layer);
 
     //EDRLR
-    map.addLayer(layer_EDRLR);  
+    map.addLayer(layer_EDRLR);
 
     map.addLayer(layer_batiment);
     layer_batiment.setVisibility(false);
 
 
-    var protocol; 
+    var protocol;
 
     if(EMBELLIS_MODE)
     {
         layer_espaces_structurants = new OpenLayers.Layer.WMS( "Espaces_structurants",
-                urlBrugis, 
+                urlBrugis,
                 {   layers: 'Espaces_structurants',
                     version:'1.3.0',
                                    transparent: "true",
                                    format: 'image/png'
                 },
                 {isBaseLayer: false}
-                );                   
+                );
 
         layer_Zichee = new OpenLayers.Layer.WMS( "Zichee",
-                urlBrugis, 
+                urlBrugis,
                 {   layers: 'Zichee',
                     version:'1.3.0',
                      transparent: "true",
                      format: 'image/png'
                 },
                 {isBaseLayer: false}
-                );                   
+                );
 
 
         map.addLayer(layer_espaces_structurants);
@@ -186,20 +186,20 @@ function init() {
         /*FILTER: 'FILTER='+filterAsString,
           filter: 'filter='+filterAsString,*/
         vendorParams:'filter=<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc"><ogc:PropertyIsBetween><ogc:PropertyName>NOTIF_COMM</ogc:PropertyName><ogc:LowerBoundary><ogc:Literal>2008-12-05</ogc:Literal></ogc:LowerBoundary><ogc:UpperBoundary><ogc:Literal>2012-12-04</ogc:Literal></ogc:UpperBoundary></ogc:PropertyIsBetween></ogc:Filter>',
-         url: urlBrugis,                     
+         url: urlBrugis,
          title: 'Identify features by clicking',
          layers: (function()
              {
-                 if(EMBELLIS_MODE) 
-         {                               
+                 if(EMBELLIS_MODE)
+         {
              return [layer,layer_EDRLR,layer_espaces_structurants,layer_Zichee];
          }
                  else
          {
-             return [layer,layer_EDRLR];                     
+             return [layer,layer_EDRLR];
          }
              })(),
-         infoFormat: 'application/vnd.ogc.gml', 
+         infoFormat: 'application/vnd.ogc.gml',
          queryVisible: true,
          eventListeners: {
 
@@ -209,12 +209,12 @@ function init() {
                  var icon = new OpenLayers.Icon('++resource++cirb.renoprime.img/marker.png', size, offset);
                  markers.clearMarkers();
 
-                 markers.addMarker(new OpenLayers.Marker(map.getLonLatFromViewPortPx(event.xy),icon));                           
+                 markers.addMarker(new OpenLayers.Marker(map.getLonLatFromViewPortPx(event.xy),icon));
 
 
                  serie=null;
-                 if (event.features && event.features.length) 
-                 {                               
+                 if (event.features && event.features.length)
+                 {
                      for(var i=0;i<event.features.length;i++)
                      {
                          //console.log(event.features[i].fid);
@@ -237,7 +237,7 @@ function init() {
                              tempDate.setMonth(dateSplit[1]);
                              tempDate.setDate(dateSplit[2]);
 
-                             tempDate.setMonth(tempDate.getMonth()-1);                                       
+                             tempDate.setMonth(tempDate.getMonth()-1);
 
                              tempDate.setDate(tempDate.getDate()-1);
                              tempDate.setFullYear(tempDate.getFullYear()+4);
@@ -255,7 +255,7 @@ function init() {
                                     serie="("+serie;
                                     */
                                  serie=infoDateSerie[LANG]+tempDate.getDate()+"/"+(tempDate.getMonth()+1)+"/"+tempDate.getFullYear()+")";
-                             }                                       
+                             }
                          }
 
                          if(EMBELLIS_MODE)
@@ -267,20 +267,20 @@ function init() {
                              }
 
                              if(event.features[i].fid.indexOf("Espace")>-1)
-                             {                                           
+                             {
                                  isEspacePresent=true;
                                  event.features[i].layerId=3;
                              }
                          }
-                     }                               
+                     }
                  }
                  else
-                 {                               
+                 {
                      isEDRLRPresent=false;
                      isQuartierPresent=false;
                      isEspacePresent=false;
                      isZicheePresent=false;
-                 }   
+                 }
 
                  if(EMBELLIS_MODE)
                  {
@@ -313,13 +313,13 @@ function init() {
        GetFeatureInfo%26EXCEPTIONS%3DINIMAGE%26BBOX%3D146817.005906%252C168626.005906%252C152466.994094%252C174275.994094%26
        FEATURE_COUNT=10%26HEIGHT%3D650%26WIDTH%3D650%26FORMAT%3Dimage%252Fpng%26INFO_FORMAT%3Dapplication%252Fvnd.ogc.gml%26CRS%3D
        EPSG%253A31370&I=498&J=258&filter=<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc"><ogc:PropertyIsBetween><ogc:PropertyName>NOTIF_COMM</ogc:PropertyName><ogc:LowerBoundary><ogc:Literal>2008-12-05</ogc:Literal></ogc:LowerBoundary><ogc:UpperBoundary><ogc:Literal>2012-12-04</ogc:Literal></ogc:UpperBoundary></ogc:PropertyIsBetween></ogc:Filter>
-     * 
-     * */               
+     *
+     * */
 
-}    
+}
 
 
-function getTileInfos(loc) 
+function getTileInfos(loc)
 {
     var res = layer.getServerResolution();
 
@@ -330,7 +330,7 @@ function getTileInfos(loc)
     var row = Math.floor(fy);
 
     return {
-        col: col, 
+        col: col,
             row: row,
             i: Math.floor((fx - col) * layer.tileSize.w),
             j: Math.floor((fy - row) * layer.tileSize.h)
@@ -339,7 +339,7 @@ function getTileInfos(loc)
 
 function bussinessLogicEspaceStructurant(event)
 {
-    /*test if the feature is whitin 8 m to the features of the layer*/ 
+    /*test if the feature is whitin 8 m to the features of the layer*/
 
     var coordinate = null;
 
@@ -359,19 +359,19 @@ function getBatiment(event,point)
             {
                 type: OpenLayers.Filter.Spatial.INTERSECTS,
         property: "GEOM",
-        value: point                        
+        value: point
             }
             );
 
     var protocol_bati = new OpenLayers.Protocol.WFS.fromWMSLayer(layer_batiment);
 
-    var response = protocol_bati.read({              
+    var response = protocol_bati.read({
         filter: filterBati,
         callback: function(result) {
             if(result.success())
-    {          
-        if(result.features.length) 
-    {   
+    {
+        if(result.features.length)
+    {
         var filterWithin = new OpenLayers.Filter.Spatial(
             {
                 type: OpenLayers.Filter.Spatial.DWITHIN,
@@ -383,19 +383,19 @@ function getBatiment(event,point)
             );
 
         protocol = new OpenLayers.Protocol.WFS.fromWMSLayer(layer_espaces_structurants);
-        var response = protocol.read({              
+        var response = protocol.read({
             filter: filterWithin,
             callback: function(result) {
-                if(result.success()) 
-        {                           
-            if(result.features.length) 
-        {                             
+                if(result.success())
+        {
+            if(result.features.length)
+        {
             //set the boolean espace_structurant to true
             isEspacePresent=true;
 
             event.features.push(result.features[0]);
             var i=event.features.length;
-            event.features[i-1].layerId=3;                                                                                                  
+            event.features[i-1].layerId=3;
         }
         }
         //display the result of the request knowing all the layers concerned
